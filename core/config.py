17190426afbfbf4ca2e4287eca6bdb84e92c63e9
rezatn0934 <rezatn0934@gmail.com>
2023-11-02 15:20:06 +0330

    chore: define Settings parameters

---
+++ core/config.py
@@ -0,0 +1,20 @@
+from pydantic_settings import BaseSettings
+
+
+class Settings(BaseSettings):
+    MONGO_URI: str
+    REDIS_HOST: str
+    REDIS_PORT: int
+    REDIS_DATABASE_NUMBER: int
+    ACCESS_TOKEN_LIFETIME: int
+    REFRESH_TOKEN_LIFETIME: int
+    SECRET_KEY: str
+    ALGORITHM: str
+
+    class Config:
+        env_file = ".env"
+        env_file_encoding = "utf-8"
+        case_sensitive = False
+
+
+settings = Settings()
