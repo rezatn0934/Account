b545d263f103d093f7d684b897358e115050af88
rezatn0934 <rezatn0934@gmail.com>
2023-11-02 15:23:08 +0330

    chore: define MONGO_DATABASE_NAME in Settings parameters

--- core/config.py
+++ core/config.py
@@ -3,6 +3,7 @@ from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
     MONGO_URI: str
+    MONGO_DATABASE_NAME: str
     REDIS_HOST: str
     REDIS_PORT: int
     REDIS_DATABASE_NUMBER: int
