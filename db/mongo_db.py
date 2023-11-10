a3087eca987ff37312a8a59f655a70dc56618d28
rezatn0934 <rezatn0934@gmail.com>
2023-11-10 12:21:33 +0330

    chore: change core name to config

--- db/mongo_db.py
+++ db/mongo_db.py
@@ -1,5 +1,5 @@
 import motor.motor_asyncio
-from core.config import settings
+from config.config import settings
 
 MONGO_URI = settings.MONGO_URI
 client = motor.motor_asyncio.AsyncIOMotorClient(MONGO_URI)
