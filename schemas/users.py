cbba6eb1a6c0b7af918d184474aeab4bafe52a73
rezatn0934 <rezatn0934@gmail.com>
2023-11-10 12:23:44 +0330

    chore: define ResetPassword schema

--- schemas/users.py
+++ schemas/users.py
@@ -1,4 +1,5 @@
-from pydantic import BaseModel, EmailStr
+from fastapi import HTTPException, status
+from pydantic import BaseModel, EmailStr, model_validator
 
 
 class UserBase(BaseModel):
@@ -6,6 +7,7 @@ class UserBase(BaseModel):
 
 
 class UserCreate(UserBase):
+    is_registered: bool = False
     password: str
     confirm_password: str
 
@@ -18,3 +20,15 @@ class UserInfo(UserBase):
     id: int
     fullname: str
 
+
+class ResetPassword(BaseModel):
+    password: str
+    confirm_password: str
+
+    @model_validator(mode='after')
+    def check_passwords_match(self) -> "ResetPassword":
+        password = self.password
+        confirm_password = self.confirm_password
+        if password is not None and password != confirm_password:
+            raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Passwords don't match")
+        return self
