b28274ef08529fb68f3e0f703e3213625e11f3e0
rezatn0934 <rezatn0934@gmail.com>
2023-11-10 12:23:12 +0330

    chore: define account required redis connections

--- db/redis_db.py
+++ db/redis_db.py
@@ -1,9 +1,33 @@
 import redis.asyncio as aioredis
 
-from core.config import settings
+from config.config import settings
+
+redis_auth = aioredis.from_url(f'redis://{settings.REDIS_HOST}:{settings.REDIS_PORT}', encoding='utf-8',
+                               db=settings.REDIS_AUTH_DATABASE, decode_responses=True)
+
+redis_token = aioredis.from_url(f'redis://{settings.REDIS_HOST}:{settings.REDIS_PORT}', encoding='utf-8',
+                                db=settings.REDIS_TOKEN_DATABASE, decode_responses=True)
 
 
-async def get_redis_client():
-    redis = await aioredis.from_url(f'redis://{settings.REDIS_HOST}:{settings.REDIS_PORT}', encoding='utf-8', db=2,
-                                    decode_response=True)
-    return redis
+def get_redis_auth_client():
+
+    """
+    The get_redis_auth_client function returns a redis client object that is connected to the Redis server.
+        The function uses the REDIS_AUTH_HOST and REDIS_AUTH_PORT environment variables to connect to the Redis server.
+
+    :return: The redis_auth variable
+    :doc-author: Trelent
+    """
+    return redis_auth
+
+
+def get_redis_token_client():
+
+    """
+    The get_redis_token_client function returns a redis client object that is connected to the Redis server.
+        This function is used by other functions in this module to connect to the Redis server.
+
+    :return: The redis_token variable
+    :doc-author: Trelent
+    """
+    return redis_token
