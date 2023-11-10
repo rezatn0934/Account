dffc3d75497b472b5ba9ef1508f55bcce7164ea6
rezatn0934 <rezatn0934@gmail.com>
2023-11-10 12:20:17 +0330

    chore: define required configs

--- config/config.py
+++ config/config.py
@@ -1,16 +1,26 @@
+from functools import lru_cache
 from pydantic_settings import BaseSettings
 
 
 class Settings(BaseSettings):
     MONGO_URI: str
     MONGO_DATABASE_NAME: str
+
     REDIS_HOST: str
     REDIS_PORT: int
-    REDIS_DATABASE_NUMBER: int
+    REDIS_AUTH_DATABASE: int
+    REDIS_TOKEN_DATABASE: int
+
     ACCESS_TOKEN_LIFETIME: int
     REFRESH_TOKEN_LIFETIME: int
     SECRET_KEY: str
     ALGORITHM: str
+
+    NOTIFICATION_REGISTER_URL: str
+    NOTIFICATION_RESET_PASSWORD_URL: str
+
+    ELASTICSEARCH_HOST: str
+    ELASTICSEARCH_PORT: int
 
     class Config:
         env_file = ".env"
@@ -18,4 +28,9 @@ class Settings(BaseSettings):
         case_sensitive = False
 
 
-settings = Settings()
+@lru_cache
+def load_settings():
+    return Settings()
+
+
+settings = load_settings()
